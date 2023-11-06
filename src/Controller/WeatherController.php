698d051a5117c615475b84d2c4cbd37ef1975d10
Bauerrr <33778131+Bauerrr@users.noreply.github.com>
2023-11-06 20:48:32 +0100

    lab E

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -6,15 +6,17 @@ use App\Entity\Weather;
 use App\Form\WeatherType;
 use App\Repository\WeatherRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/weather')]
 class WeatherController extends AbstractController
 {
     #[Route('/', name: 'app_weather_index', methods: ['GET'])]
+    #[IsGranted('ROLE_WEATHER_INDEX')]
     public function index(WeatherRepository $weatherRepository): Response
     {
         return $this->render('weather/index.html.twig', [
@@ -23,6 +25,7 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/new', name: 'app_weather_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_WEATHER_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $weather = new Weather();
@@ -45,6 +48,7 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_weather_show', methods: ['GET'])]
+    #[IsGranted('ROLE_WEATHER_SHOW')]
     public function show(Weather $weather): Response
     {
         return $this->render('weather/show.html.twig', [
@@ -53,6 +57,7 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_weather_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_WEATHER_EDIT')]
     public function edit(Request $request, Weather $weather, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(WeatherType::class, $weather, [
@@ -73,6 +78,7 @@ class WeatherController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_weather_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_WEATHER_DELETE')]
     public function delete(Request $request, Weather $weather, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$weather->getId(), $request->request->get('_token'))) {
